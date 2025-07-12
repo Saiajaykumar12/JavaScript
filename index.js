bf8b6fd8b4db458a4e11c55d3047caeeee680770
Ajay <saiajay1214@gmail.com>
2025-07-12 18:03:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// a = 10;
+// console.log(a);
+
+// A variable is a container used to store data values.
+// It allows you to reuse,manipulate data in a program.
+
+// var a = 25;
+// console.log(a);
+
+// let a = 20; // assigned and declared
+// a = 30;     // reassigned and redeclared
+// console.log(a);
+
+// const a = 40; // declared and assigned
+// a = 50; // redeclared and reassigned;     Throws error
+
+// console.log(a);
+// var a = 40;           undefined
+
+// let a=1,b=2,c=3;
+// console.log(a,b,c)       We candeclare multiple variables in a single line
+
+// let a;
+// console.log(a);
+
+// console.log(a);
+// let a = 10;
+
+// console.log(a);
+// const a = 10;
+
+console.log(a);
+const a = 40;
+
