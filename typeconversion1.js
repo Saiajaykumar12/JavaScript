6e6167a755e08d0c7a553f7e606221c7e5f6619c
Ajay <saiajay1214@gmail.com>
2025-07-21 22:21:11 +0530

    Typeconversion

---
+++ typeconversion1.js
@@ -0,0 +1,39 @@
+
+// 1. Number()
+
+
+// let a = "10";
+// a = Number(a);
+// console.log(a);  10
+// let c = "123ab"
+// c = Number(c);
+// console.log(c);  //NaN
+
+// 2. using +
+
+// let b = "30ab";
+// b = +"30ab";
+// console.log(b);    //NaN
+
+// 3. parseInt()
+
+// let d = "40abc";
+// d = parseInt(d);
+// console.log(d);    //40                 It will read from left to right
+
+// 4. parseFloat
+
+// let e = "1332.432abc"
+// e = parseFloat(e);
+// console.log(e);     // 1332.432
+
+
+// let f = "de12099"
+// f = parseInt(f);
+// console.log(f);    //NaN
+
+
+
+
+
+
